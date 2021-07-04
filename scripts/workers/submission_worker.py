76d8d505b1a0db241c31cb0c5ea7d06423111373
Aviral Jain <avi.aviral140@gmail.com>
2021-07-04 20:43:24 +0530

    change counters name for queue monitoring

--- scripts/workers/submission_worker.py
+++ scripts/workers/submission_worker.py
@@ -689,39 +689,16 @@ def process_add_challenge_message(message):
     extract_challenge_data(challenge, phases)
 
 
-def push_metrics_to_pushgateway(submission_pk, queue_name):
-    try:
-        registry = CollectorRegistry()
-        submissions_unloaded_from_queue = Counter(
-            "submissions_unloaded_from_queue",
-            "Submissions unloaded from queue",
-            ["submission_pk", "queue_name"],
-            registry=registry,
-        )
-        submissions_unloaded_from_queue.labels(submission_pk, queue_name).inc()
-        pushgateway_endpoint = os.environ.get("PUSHGATEWAY_ENDPOINT")
-        job_id = "submission_worker_{}".format(submission_pk)
-        pushadd_to_gateway(pushgateway_endpoint, job=job_id, registry=registry)
-    except Exception as e:
-        logger.exception(
-            "{} Exception when pushing metrics to push gateway: {}".format(
-                SUBMISSION_LOGS_PREFIX, e
-            )
-        )
-
-
-def process_submission_callback(body, queue_name):
+def process_submission_callback(body):
     try:
         logger.info(
             "{} [x] Received submission message {}".format(
                 SUBMISSION_LOGS_PREFIX, body
             )
         )
-        submission_pk = json.loads(body)["submission_pk"]
         body = yaml.safe_load(body)
         body = dict((k, int(v)) for k, v in body.items())
         process_submission_message(body)
-        push_metrics_to_pushgateway(submission_pk, queue_name)
     except Exception as e:
         logger.exception(
             "{} Exception while receiving message from submission queue with error {}".format(
@@ -866,7 +843,7 @@ def main():
                                 WORKER_LOGS_PREFIX, message.body
                             )
                         )
-                        process_submission_callback(message.body, queue_name)
+                        process_submission_callback(message.body)
                         # Let the queue know that the message is processed
                         message.delete()
                         increment_and_push_metrics_to_pushgateway(message.body, queue_name)
@@ -876,7 +853,7 @@ def main():
                             WORKER_LOGS_PREFIX, message.body
                         )
                     )
-                    process_submission_callback(message.body, queue_name)
+                    process_submission_callback(message.body)
                     # Let the queue know that the message is processed
                     message.delete()
                     increment_and_push_metrics_to_pushgateway(message.body, queue_name)
@@ -895,7 +872,7 @@ def main():
                             WORKER_LOGS_PREFIX, message.body
                         )
                     )
-                    process_submission_callback(message.body, queue_name)
+                    process_submission_callback(message.body)
                     # Let the queue know that the message is processed
                     message.delete()
                     increment_and_push_metrics_to_pushgateway(message.body, queue_name)
